347dc40c78539c013429d48779a8150b9836b816
visalirey <69970884+visalirey@users.noreply.github.com>
2021-01-09 18:09:38 +0530

    move to folder

---
+++ test_data.py
@@ -0,0 +1,26 @@
+import unittest
+from  unittest.mock import patch
+import data   # import your file (give your file name)
+import json
+
+class TestDataExtraction(unittest.TestCase):
+
+    def test_data(self):
+        fake_json = [{'login':"visali"}]
+
+        with patch('data.requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.json.return_value = fake_json
+
+            obj1 = data. Data_Extraction()
+            response = obj1.get
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), fake_json)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
